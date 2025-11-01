63cd4a31e9da4739b47a1620a5f27d6a0edf1a7c
Boity <mrsboitymbhele@gmail.com>
2025-11-01 21:37:14 +0200

    Re-added grade symbol generator as regular folder

--- letter.py
+++ letter.py
@@ -1,1 +1,34 @@
+# Write a function called letter_grade(mark:int) -> str that returns a letter a student got:
+# ○ A = 80–100
+# ○ B = 70–79
+# ○ C = 60–69
+# ○ D = 50–59
+# ○ F = below 50
+# Write unit tests to test if the function returns the expected.
+# the file structure of the program must be
+# letter.py
+# test_letter.py
+
+def letter_grade(mark):
+    """_Function returns a specific letter according to the grades of a student_
+
+    Args:
+        mark (_integer_): _The Mark range of a the students_
+
+    Returns:
+        _Str_: _A letter associated with the marks_
+    """
+    mark = int(mark)
+    if mark >= 80:
+        return "A"
+    if mark >= 70:
+        return "B"
+    if mark >= 60:
+        return "C"
+    if mark >= 50:
+        return "D"
+    else:
+        return "F"
+#marks = input("Enter your marks: ")
+print(letter_grade(85))
 
--- test_letter.py
+++ test_letter.py
@@ -1,1 +1,30 @@
+import unittest
+from letter import letter_grade
+
+class TestLetterGrade(unittest.TestCase):
+
+    def test_grade_A(self):
+        self.assertEqual(letter_grade(85), "A")
+        print("You got an A")
+
+    def test_grade_B(self):
+        self.assertEqual(letter_grade(75), "B")
+        print("You got a B")
+
+    def test_grade_C(self):
+        self.assertEqual(letter_grade(65), "C")
+        print("You got a C")
+
+    def test_grade_D(self):
+        self.assertEqual(letter_grade(55), "D")
+        print("You got a D")
+
+    def test_grade_F(self):
+        self.assertEqual(letter_grade(45), "F")
+        print("You got a F")
+
+
+if __name__ == '__main__':
+    unittest.main()
+       
 
